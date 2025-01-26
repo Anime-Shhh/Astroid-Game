597a810ebcd2890cc62aebdfb643d9e9df766d71
Anime-Shhh <animeshjadhav05@gmail.com>
2025-01-26 18:16:01 -0500

    user movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,12 +9,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+
     while True:
+        # to exit the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = game_clock.tick(180)/1000
 
 
 if __name__ == "__main__":
